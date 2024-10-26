ec0ac42364bc7a33c725a1c4b78c83ead7cab19d
Ren Chen <xuanrenchen7@gmail.com>
2024-10-26 16:44:44 -0400

    Implemented posts route

--- app.py
+++ app.py
@@ -1,26 +1,58 @@
-from flask import Flask, render_template, make_response
+from flask import Flask, request, render_template, make_response
 from utils.db import connect_db
+from utils.posts import get_post, create_post
 
 app = Flask(__name__)
 
-@app.route('/login', methods=['GET'])
+db = connect_db()
+if db is not None:
+    print('databse connected successfully')
+    # app.logger.info('databse connected successfully')
+else:
+    print('databse not connected')
+    # app.logger.info('databse not connected')
+
+@app.route('/login', methods=['GET','POST'])
 def login():
-    response = make_response(render_template('login.html'))
-    response.headers['X-Content-Type-Options'] = 'nosniff'
-    return response
+    if request.method == 'GET':
+        response = make_response(render_template('login.html'))
+        response.headers['X-Content-Type-Options'] = 'nosniff'
+        return response
+        #respond with the html
+    if request.method == 'POST':
+        #do post implementation
+        pass
 
 
-
-@app.route('/home', methods=['GET'])
+@app.route('/', methods=['GET'])
 def home():
-    db = connect_db()
-    if db is not None:
-        app.logger.info('databse connected successfully')
-    else:
-        app.logger.info('databse not connected')
     response = make_response(render_template('home_page.html'))
+    response.mimetype = "text/html"
     response.headers['X-Content-Type-Options'] = 'nosniff'
     return response
+
+@app.route('/posts', methods=['GET','POST'])
+def posts():
+    if request.method == 'GET':
+        posts = get_post()
+        response = make_response()
+        response.set_data(posts)
+        response.mimetype = "application/json"
+        response.headers['X-Content-Type-Options'] = 'nosniff'
+        return response
+    
+    if request.method == 'POST':
+        create_post(request)
+        response = make_response()
+        response.headers['X-Content-Type-Options'] = 'nosniff'
+        return response
+        
+
+@app.route('/posts/<string:post_id>', methods=['DELETE'])
+def delete_posts(post_id):
+    #implement delete post
+    pass
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
+
---
+++ static/functions.js
@@ -0,0 +1,10 @@
+function updateHome() {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(JSON.parse(this.response));
+        }
+    }
+    request.open("GET", "/posts");
+    request.send();
+}
