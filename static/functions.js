f5f1359e76268854a8e69c7b955858c4b64c7c01
Ren Chen <xuanrenchen7@gmail.com>
2024-11-30 05:10:23 -0500

    Removed print statement

--- static/functions.js
+++ static/functions.js
@@ -42,7 +42,6 @@ function getPosts() {
     const request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
-            console.log(JSON.parse(this.responseText))
             updatePosts(JSON.parse(this.responseText));
         }
     };
