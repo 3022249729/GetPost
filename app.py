bf00677dbcfe47af5ed2d7b62c89e59d44e0ae3f
WhyIsItSoHardToFindANotUsedName <chenciliang123@gmail.com>
2024-12-01 14:29:30 -0500

    ws for post and likes

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
-from flask import Flask, render_template, make_response, request, redirect, url_for, flash, send_from_directory
-=======
 import eventlet
 
 eventlet.monkey_patch()
 
 from flask import Flask, render_template, make_response, request, redirect, url_for, flash, send_from_directory
 from flask_socketio import SocketIO, send, emit
->>>>>>> d8dadfd (websocket-liked-comments)
 from utils.db import connect_db
 from utils.login import validate_password
 from utils.posts import get_post, create_post, delete_post
@@ -17,11 +13,6 @@ import secrets
 import bcrypt
 import os
 from bson import ObjectId
-<<<<<<< HEAD
-
-app = Flask(__name__)
-app.secret_key = secrets.token_hex(16)
-=======
 import html
 import json
 from datetime import datetime
@@ -31,7 +22,6 @@ ws = True
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
 socketio = SocketIO(app)
->>>>>>> d8dadfd (websocket-liked-comments)
 
 db = connect_db()
 if db is not None:
@@ -47,48 +37,32 @@ def serve_css(filename):
     response.mimetype = "text/css"
     return response
 
-<<<<<<< HEAD
-=======
 
->>>>>>> d8dadfd (websocket-liked-comments)
 @app.route('/js/<path:filename>', methods=['GET'])
 def serve_js(filename):
     response = make_response(send_from_directory('static', filename))
     response.mimetype = "text/javascript"
     return response
 
-<<<<<<< HEAD
-=======
 
->>>>>>> d8dadfd (websocket-liked-comments)
 @app.route('/images/<path:filename>', methods=['GET'])
 def serve_image(filename):
     response = make_response(send_from_directory('static/images', filename))
     return response
-<<<<<<< HEAD
-#functions to serve static files
-=======
 
 
 # functions to serve static files
->>>>>>> d8dadfd (websocket-liked-comments)
 
 
 @app.after_request
 def add_nosniff_header(response):
     response.headers['X-Content-Type-Options'] = 'nosniff'
     return response
-<<<<<<< HEAD
-#function to add nosniff to every response
-
-@app.route('/login', methods=['GET','POST'])
-=======
 
 
 # function to add nosniff to every response
 
 @app.route('/login', methods=['GET', 'POST'])
->>>>>>> d8dadfd (websocket-liked-comments)
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
@@ -110,11 +84,7 @@ def login():
         response.set_cookie('auth_token', auth_token, httponly=True, max_age=3600)
         response.mimetype = "text/html"
         return response
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> d8dadfd (websocket-liked-comments)
     if request.method == 'GET':
         auth_token = request.cookies.get('auth_token')
         if auth_token:
@@ -123,11 +93,7 @@ def login():
                 response = make_response(redirect('/'))
                 response.mimetype = "text/html"
                 return response
-<<<<<<< HEAD
-            
-=======
 
->>>>>>> d8dadfd (websocket-liked-comments)
         response = make_response(render_template('login.html'))
         response.set_cookie('auth_token', '', expires=0)
         response.mimetype = "text/html"
@@ -150,13 +116,9 @@ def register():
 
         # Check if password meets strength requirements
         if not validate_password(password):
-<<<<<<< HEAD
-            flash("Password does not meet the requirements:<br>- 8+ characters<br>- 1 lowercase letter<br>- 1 uppercase letter<br>- 1 special character: !,@,#,$,%,^,&,(,),-,_,=", "error")
-=======
             flash(
                 "Password does not meet the requirements:<br>- 8+ characters<br>- 1 lowercase letter<br>- 1 uppercase letter<br>- 1 special character: !,@,#,$,%,^,&,(,),-,_,=",
                 "error")
->>>>>>> d8dadfd (websocket-liked-comments)
             response = make_response(render_template('register.html'))
             response.mimetype = "text/html"
             return response
@@ -204,27 +166,16 @@ def home():
         response = make_response(redirect(url_for("login"), 302))
         response.mimetype = "text/html"
         return response
-<<<<<<< HEAD
-    #if not logged in redirect back to login
-    
-    auth_token = request.cookies.get("auth_token")
-    user = credential_collection.find_one({"auth_token_hash":hashlib.sha256(auth_token.encode()).hexdigest()})
-=======
     # if not logged in redirect back to login
 
     auth_token = request.cookies.get("auth_token")
     user = credential_collection.find_one({"auth_token_hash": hashlib.sha256(auth_token.encode()).hexdigest()})
->>>>>>> d8dadfd (websocket-liked-comments)
     if not user:
         response = make_response(redirect(url_for("login"), 302))
         response.set_cookie('auth_token', '', expires=0)
         response.mimetype = "text/html"
         return response
-<<<<<<< HEAD
-    #if using invalid auth_token, redirect back to login
-=======
     # if using invalid auth_token, redirect back to login
->>>>>>> d8dadfd (websocket-liked-comments)
 
     username = user['username']
     pfp = "default.png"
@@ -236,11 +187,7 @@ def home():
     return response
 
 
-<<<<<<< HEAD
-@app.route('/posts', methods=['GET','POST'])
-=======
 @app.route('/posts', methods=['GET', 'POST'])
->>>>>>> d8dadfd (websocket-liked-comments)
 def posts():
     if request.method == 'GET':
         posts = get_post(db, request)
@@ -253,15 +200,6 @@ def posts():
         if code == 403:
             response = make_response("Permission Denied", 403)
             response.mimetype = "text/plain"
-<<<<<<< HEAD
-            return response  
-
-        elif code == 200:
-            response = make_response('', 200) 
-            response.mimetype = "text/plain"
-            return response  
-        
-=======
             return response
 
         elif code == 200:
@@ -269,7 +207,6 @@ def posts():
             response.mimetype = "text/plain"
             return response
 
->>>>>>> d8dadfd (websocket-liked-comments)
 
 @app.route('/posts/<string:post_id>', methods=['DELETE'])
 def delete_posts(post_id):
@@ -278,47 +215,28 @@ def delete_posts(post_id):
     if code == 403:
         response = make_response("Permission Denied", 403)
         response.mimetype = "text/plain"
-<<<<<<< HEAD
-        return response  
-=======
         return response
->>>>>>> d8dadfd (websocket-liked-comments)
 
     elif code == 204:
         response = make_response("No Content", 204)
         response.mimetype = "text/plain"
-<<<<<<< HEAD
-        return response  
-    
-=======
         return response
 
 
->>>>>>> d8dadfd (websocket-liked-comments)
 @app.route('/like/<string:post_id>', methods=['POST'])
 def like_post(post_id):
     auth_token = request.cookies.get("auth_token")
     if not auth_token:
         response = make_response("Permission Denied", 403)
         response.mimetype = "text/plain"
-<<<<<<< HEAD
-        return response  
-    
-=======
         return response
 
->>>>>>> d8dadfd (websocket-liked-comments)
     user = credential_collection.find_one({"auth_token_hash": hashlib.sha256(auth_token.encode()).hexdigest()})
     if not user:
         response = make_response("Permission Denied", 403)
         response.mimetype = "text/plain"
-<<<<<<< HEAD
-        return response  
-    
-=======
         return response
 
->>>>>>> d8dadfd (websocket-liked-comments)
     post_collection = db["posts"]
     post_collection.update_one(
         {"_id": ObjectId(post_id)},
@@ -329,36 +247,22 @@ def like_post(post_id):
     response.mimetype = "text/plain"
     return response
 
-<<<<<<< HEAD
-=======
 
->>>>>>> d8dadfd (websocket-liked-comments)
 @app.route('/setpfp/<string:image_name>', methods=['POST'])
 def setpfp(image_name):
     auth_token = request.cookies.get("auth_token")
     if not auth_token:
         response = make_response("Permission Denied", 403)
         response.mimetype = "text/plain"
-<<<<<<< HEAD
-        return response  
-    
-=======
         return response
 
->>>>>>> d8dadfd (websocket-liked-comments)
     user = credential_collection.find_one({"auth_token_hash": hashlib.sha256(auth_token.encode()).hexdigest()})
     if not user:
         response = make_response("Permission Denied", 403)
         response.mimetype = "text/plain"
-<<<<<<< HEAD
-        return response 
-    #verify user
-    
-=======
         return response
         # verify user
 
->>>>>>> d8dadfd (websocket-liked-comments)
     credential_collection.update_one(
         {"_id": ObjectId(user["_id"])},
         {"$set": {"pfp": image_name}}
@@ -368,34 +272,21 @@ def setpfp(image_name):
     response.mimetype = "text/html"
     return response
 
-<<<<<<< HEAD
-=======
 
->>>>>>> d8dadfd (websocket-liked-comments)
 @app.route('/uploadpfp', methods=['POST'])
 def uploadpfp():
     auth_token = request.cookies.get("auth_token")
     if not auth_token:
         response = make_response("Permission Denied", 403)
         response.mimetype = "text/plain"
-<<<<<<< HEAD
-        return response  
-    
-=======
         return response
 
->>>>>>> d8dadfd (websocket-liked-comments)
     user = credential_collection.find_one({"auth_token_hash": hashlib.sha256(auth_token.encode()).hexdigest()})
     if not user:
         response = make_response("Permission Denied", 403)
         response.mimetype = "text/plain"
-<<<<<<< HEAD
-        return response 
-    #verify user
-=======
         return response
         # verify user
->>>>>>> d8dadfd (websocket-liked-comments)
 
     file = request.files['pfp']
     file_bytes = file.read()
@@ -403,11 +294,7 @@ def uploadpfp():
     extension = get_file_extension(file_bytes)
     if extension == None or extension == "mp4":
         return {'success': False, 'message': 'Unsupported file type.'}, 400
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> d8dadfd (websocket-liked-comments)
     filename = os.urandom(16).hex()
     file_path = os.path.join("./static/images", f"{filename}.{extension}")
     with open(file_path, 'wb') as f:
@@ -421,11 +308,6 @@ def uploadpfp():
     return {'success': True}, 200
 
 
-<<<<<<< HEAD
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
-
-=======
 @socketio.on('message')
 def handle_websocket_message(str_data):
     json_data = json.loads(str_data)
@@ -481,4 +363,4 @@ if __name__ == "__main__":
         socketio.run(app, host='0.0.0.0', port=8080)
     else:
         app.run(host='0.0.0.0', port=8080, debug=True)
->>>>>>> d8dadfd (websocket-liked-comments)
+
